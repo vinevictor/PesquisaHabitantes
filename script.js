8736e37df96204cf299d96f99a77182dc0cec669
Vinicius Victor <vine.victordev@gmail.com>
2022-11-06 21:48:52 -0300

    Função Media dos Salarios

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ function PegaInformacoes() {
     salarios.push(parseInt(prompt("Salario:")))
     numDeFilhos.push(parseInt(prompt("Number de filhos:")));
 };
+
+function MediaDeSalarios() {
+    let somaSalarios = 0
+    salarios.forEach(numero => {
+        somaSalarios = somaSalarios + numero;
+    });
+    let mediaDeSalarios = somaSalarios / salarios.length;
+    console.log("Media de salarios dos habitantes: " + mediaDeSalarios)
+};
+
+// while para testes
+let repeat = true;
+while (repeat) {
+    PegaInformacoes();
+    let continuar = prompt("");
+    if (continuar == "n") {
+        repeat = false;
+    }
+};
+
